b4ac71fcc7320a8d42d441e304bb459cea79e5ab
sebastian <35818322+SebastianRemander@users.noreply.github.com>
2022-08-01 10:07:51 +0300

    Add Consumer Producer abstractions, demo topics.
    Consumer and Producer have an async create method to handle
    "initialization" that requires awaiting.

---
+++ src/streaming.py
@@ -0,0 +1,51 @@
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+
+
+KAFKA_HOST = "kafka:9092"
+
+
+class Consumer(AIOKafkaConsumer):
+
+    def __init__(self, *args, **kwargs):
+        return super().init(
+            *args,
+            **kwargs,
+            bootstrap_servers=KAFKA_HOST,
+        )
+
+    @classmethod
+    async def create(cls, *args, **kwargs):
+        consumer = cls(*args, **kwargs)
+        await consumer.start()
+        return consumer
+
+
+class Producer(AIOKafkaProducer):
+
+    def __init__(self, *args, **kwargs):
+        return super().init(
+            *args,
+            **kwargs,
+            bootstrap_servers=KAFKA_HOST,
+        )
+
+    @classmethod
+    async def create(cls, *args, **kwargs):
+        producer = cls(*args, **kwargs)
+        await producer.start()
+        return producer
+
+
+class Topic(str, Enum):
+    """
+    Recommended topic naming structure:
+    <data-center>.<domain>.<classification>.<description>.<version>
+    https://devshawn.com/blog/apache-kafka-topic-naming-conventions/
+
+    (Data center is being omitted to simplify demo).
+    """
+    CRAWL = "website.fct.crawl.0"
+    CHANGE = "website.cdc.change.0"
+    PAGERANK = "website.cdc.pagerank.0"
+    NER = "website.cdc.ner.0"
+    RELEVANCY = "website.cdc.relevancy.0"
